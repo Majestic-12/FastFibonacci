4fa3d811538952bb6e18eb5b0823b923e06f1c61
Dan Park <dpark@magicpoint.info>
2015-03-11 22:17:55 -0700

    viewDidAppear

--- FastFibonacci/ViewController.swift
+++ FastFibonacci/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
 
 }
 
